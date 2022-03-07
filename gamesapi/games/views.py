ada0493157ac67effab8c71c02ef36c5ed39b17b
Richard Tae <taerichard@gmail.com>
2022-03-06 22:18:03 -0800

    replaced all games with dogs/breeds. added fields for serilizer, migrated db

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -1,99 +1,51 @@
-"""
-Book: Building RESTful Python Web Services
-"""
-from games.models import GameCategory
-from games.models import Game
-from games.models import Player
-from games.models import PlayerScore
+
 from games.models import Dog
 from games.models import Breed
-from games.serializers import BreedSerializer, GameCategorySerializer, DogSerializer
-from games.serializers import GameSerializer
-from games.serializers import PlayerSerializer
-from games.serializers import PlayerScoreSerializer
+from games.serializers import BreedSerializer, DogSerializer
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 class BreedList(generics.ListCreateAPIView):
-    # queryset = Breed.objects.all()
-    # serializer_class = BreedSerializer
-     name = 'breed-list'
-     def get(self, request, format=None):
-        breeds = Breed.objects.all()
-        serializer = BreedSerializer(breeds, many=True)
-        return Response(serializer.data)
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+    name = 'breed-list'
+
+    #  def get(self, request, format=None):
+    #     breeds = Breed.objects.all()
+    #     serializer = BreedSerializer(breeds, many=True)
+    #     return Response(serializer.data)
+
+    #  def get(self, request, *args, **kwargs):
+    #     return self.list(request, *args, **kwargs)
     
-     def post(self, request, format=None):
-        serializer = BreedSerializer(data=request.data)
-        print('HIGM')
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #  def post(self, request, format=None):
+    #     serializer = BreedSerializer(data=request.data)
+    #     print('HIGM')
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DogList(generics.ListCreateAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
     name = 'dog-list'
 
+class DogDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Dog.objects.all()
+  serializer_class = DogSerializer
+  name = 'dog-detail'
 
-class GameCategoryList(generics.ListCreateAPIView):
-    queryset = GameCategory.objects.all()
-    serializer_class = GameCategorySerializer
-    name = 'gamecategory-list'
-
-
-class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = GameCategory.objects.all()
-    serializer_class = GameCategorySerializer
-    name = 'gamecategory-detail'
-
-
-class GameList(generics.ListCreateAPIView):
-    queryset = Game.objects.all()
-    serializer_class = GameSerializer
-    name = 'game-list'
-
-
-class GameDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Game.objects.all()
-    serializer_class = GameSerializer
-    name = 'game-detail'
-
-
-class PlayerList(generics.ListCreateAPIView):
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
-    name = 'player-list'
-
-
-class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
-    name = 'player-detail'
-
-
-class PlayerScoreList(generics.ListCreateAPIView):
-    queryset = PlayerScore.objects.all()
-    serializer_class = PlayerScoreSerializer
-    name = 'playerscore-list'
-
-
-class PlayerScoreDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = PlayerScore.objects.all()
-    serializer_class = PlayerScoreSerializer
-    name = 'playerscore-detail'
-
+class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Breed.objects.all()
+  serializer_class = BreedSerializer
+  name = 'breed-detail'
 
 class ApiRoot(generics.GenericAPIView):
     name = 'api-root'
     def get(self, request, *args, **kwargs):
         return Response({
-            'players': reverse(PlayerList.name, request=request),
-            'game-categories': reverse(GameCategoryList.name, request=request),
-            'games': reverse(GameList.name, request=request),
-            'scores': reverse(PlayerScoreList.name, request=request),
             'breeds':reverse(BreedList.name, request=request),
             'dogs':reverse(DogList.name, request=request),
             })
